de52e65da859575bd2c1275b23feb50f132faa79
VitaliiKablukov <vetal.kablukov98@gmail.com>
2023-12-01 20:23:02 +0200

    change output token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,33 +1,34 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
-import * as argon2 from 'argon2';
-import { JwtService } from '@nestjs/jwt';
-import { IUser } from 'src/types/types';
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { UserService } from 'src/user/user.service'
+import * as argon2 from 'argon2'
+import { JwtService } from '@nestjs/jwt'
+import { IUser } from 'src/types/types'
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly usersService: UserService,
-    private readonly jwtService: JwtService,
-  ) {}
+	constructor(
+		private readonly usersService: UserService,
+		private readonly jwtService: JwtService,
+	) {}
 
-  async validateUser(email: string, password: string) {
-    const user = await this.usersService.findOne(email);
+	async validateUser(email: string, password: string) {
+		const user = await this.usersService.findOne(email)
 
-    const passwordIsMatch = await argon2.verify(user.password, password);
+		const passwordIsMatch = await argon2.verify(user.password, password)
 
-    if (user && passwordIsMatch) {
-      return user;
-    }
-    throw new UnauthorizedException('User or password incorrect');
-  }
+		if (user && passwordIsMatch) {
+			return user
+		}
+		throw new UnauthorizedException('User or password incorrect')
+	}
 
-  async login(user: IUser) {
-    const { id, email } = user;
-    return {
-      id,
-      email,
-      token: this.jwtService.sign({ id, email }),
-    };
-  }
+	async login(user: IUser) {
+		const { id, email } = user
+		return {
+			id,
+			email,
+
+			token: this.jwtService.sign({ id, email }),
+		}
+	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,47 +1,47 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
-import * as argon2 from 'argon2';
-import { JwtService } from '@nestjs/jwt';
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from './entities/user.entity'
+import { Repository } from 'typeorm'
+import * as argon2 from 'argon2'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(User)
-    private readonly usersRepository: Repository<User>,
-    private readonly jwtService: JwtService,
-  ) {}
-  async create(createUserDto: CreateUserDto) {
-    const existUser = await this.usersRepository.findOne({
-      where: { email: createUserDto.email },
-    });
-    if (existUser) throw new BadRequestException('This email already exist');
-    const user = await this.usersRepository.save({
-      email: createUserDto.email,
-      password: await argon2.hash(createUserDto.password),
-      avatar: createUserDto.avatar && '',
-    });
-    const token = this.jwtService.sign(createUserDto.email);
+	constructor(
+		@InjectRepository(User)
+		private readonly usersRepository: Repository<User>,
+		private readonly jwtService: JwtService,
+	) {}
+	async create(createUserDto: CreateUserDto) {
+		const existUser = await this.usersRepository.findOne({
+			where: { email: createUserDto.email },
+		})
+		if (existUser) throw new BadRequestException('This email already exist')
+		const user = await this.usersRepository.save({
+			email: createUserDto.email,
+			password: await argon2.hash(createUserDto.password),
+			avatar: createUserDto.avatar && '',
+		})
+		const token = this.jwtService.sign({ email: createUserDto.email })
 
-    return { user, token };
-  }
+		return { user, token }
+	}
 
-  async findOne(email: string) {
-    const existUser = this.usersRepository.findOne({ where: { email } });
-    return existUser;
-  }
-  findOneById(id: number) {
-    return `This action returns a #${id} user`;
-  }
+	async findOne(email: string) {
+		const existUser = this.usersRepository.findOne({ where: { email } })
+		return existUser
+	}
+	findOneById(id: number) {
+		return `This action returns a #${id} user`
+	}
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+	update(id: number, updateUserDto: UpdateUserDto) {
+		return `This action updates a #${id} user`
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+	remove(id: number) {
+		return `This action removes a #${id} user`
+	}
 }
